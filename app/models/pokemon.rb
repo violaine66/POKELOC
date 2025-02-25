621c4f388fd86dc9e7cbd9c6ee81cc0c5fa29785
mael leibinger <maelleibinger@gmail.com>
2025-02-25 22:32:56 +0100

    test

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,8 +1,9 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
-  
+
   validates :name, presence:true
   validates :element_type, presence:true
   validates :address, presence:true
   validates :price_per_day, presence:true
+  # c'est un test
 end
