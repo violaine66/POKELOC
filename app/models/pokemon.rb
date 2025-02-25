90d6f2ea5e891abac909073157b813a1425cb64f
violaine soulas <vhsoulas@gmail.com>
2025-02-25 21:09:35 +0100

    begin model pokemon

---
+++ app/models/pokemon.rb
@@ -0,0 +1,8 @@
+class Pokemon < ApplicationRecord
+  belongs_to :user
+  
+  validates :name, presence:true
+  validates :element_type, presence:true
+  validates :address, presence:true
+  validates :price_per_day, presence:true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_25_111411) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_25_140521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pokemons", force: :cascade do |t|
+    t.string "name"
+    t.string "element_type"
+    t.string "address"
+    t.integer "price_per_day"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pokemons_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_25_111411) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pokemons", "users"
 end
