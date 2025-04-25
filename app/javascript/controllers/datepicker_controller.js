07f2e49d129bb7de7ec81b0958362d9b1fe0a9ff
violaine soulas <vhsoulas@gmail.com>
2025-04-25 15:49:53 +0200

    Dates désactivées si réservation

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,9 +1,22 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static values = { bookedDates: Array }
+
   connect() {
-    flatpickr(this.element)
+    const unavailableDates = this.bookedDatesValue.map(date => new Date(date)); // Transformer les dates en objets Date
+
+    // Initialisation de flatpickr sur l'élément (le div parent)
+    flatpickr(this.element.querySelectorAll("input[type='text']"), {
+      disable: unavailableDates.map(date => date.toISOString().split('T')[0]), // Convertir les dates en "YYYY-MM-DD"
+      onDayCreate: (dObj, dStr, fp, dayElem) => {
+        const date = new Date(dayElem.dateObj);  // Créer un objet Date pour comparer les dates
+        if (unavailableDates.some(unavailableDate => unavailableDate.getTime() === date.getTime())) {
+          dayElem.classList.add("reserved"); // Ajouter la classe reserved si la date est réservée
+        }
+      }
+    });
   }
 }
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,4 @@
+
 class Booking < ApplicationRecord
   belongs_to :pokemon
   belongs_to :user
@@ -5,8 +6,15 @@ class Booking < ApplicationRecord
 
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates_comparison_of :end_date, greater_than_or_equal_to: :start_date,
-                          message: "Cannot be earlier than the start date!"
-
+  validates_comparison_of :end_date, greater_than_or_equal_to: :start_date, message: "Cannot be earlier than the start date!"
   validates :total_price, presence: true
+
+  validate :no_overlap_with_existing_bookings
+
+  private
+
+  def no_overlap_with_existing_bookings
+    conflicting_booking = pokemon.bookings.where('start_date < ? AND end_date > ?', end_date, start_date).exists?
+    errors.add(:base, 'Booking conflicts with an existing reservation') if conflicting_booking
+  end
 end
